57507f9e41b69b415229dd648dd8bc348ea99a24
RidgwayA <52265069+RidgwayA@users.noreply.github.com>
2025-02-11 14:49:43 -0600

    --Inital Commit-- Responsive Navbar completed.

---
+++ ar-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavbarMain from './componets/navbar/NavbarMain'
+
+function App() {
+  return (
+    // **TODO** Fix fonts maybe something fancy
+<main className='font-body'>
+  <NavbarMain/>
+</main>
+  )
+}
+
+export default App
