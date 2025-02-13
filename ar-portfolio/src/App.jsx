5db6966c8ba7e52fdb19b515f6ced1452ff28e66
RidgwayA <52265069+RidgwayA@users.noreply.github.com>
2025-02-13 12:44:30 -0600

    Added skills section complete with a description and proficent languages

--- ar-portfolio/src/App.jsx
+++ ar-portfolio/src/App.jsx
@@ -4,6 +4,8 @@ import HeroMain from './componets/heroSection/HeroMain'
 import HeroBackground from './componets/heroSection/HeroBackground'
 import HeroFooter from './componets/heroSection/HeroFooter'
 import AboutMeMain from './componets/aboutMeSection/AboutMeMain'
+import SkillMain from './componets/skillSection/SkillMain'
+import SkillFooter from './componets/skillSection/SkillFooter'
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
   <HeroBackground/>
   <HeroFooter/>
   <AboutMeMain/>
+  <SkillMain/>
+  <SkillFooter/>
 </main>
   )
 }
